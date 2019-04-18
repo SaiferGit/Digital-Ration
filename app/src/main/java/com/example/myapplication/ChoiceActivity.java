daf58a2217b5b0a30f52ae7c429fb74078ba064a
SaiferGit <44334473+SaiferGit@users.noreply.github.com>
2019-04-19 05:06:29 +0600

    choice activity added
    2nd Activity

--- app/src/main/java/com/example/myapplication/ChoiceActivity.java
+++ app/src/main/java/com/example/myapplication/ChoiceActivity.java
@@ -2,12 +2,19 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ChoiceActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+
+        //adding toolbar
+        //mToolbar =(Toolbar) findViewById(R.id.choice_toolbar);
+
     }
 }
