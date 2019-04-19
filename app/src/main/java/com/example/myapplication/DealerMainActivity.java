a8c7de31380917166d9f5beb22d67ae9370fda9b
SaiferGit <44334473+SaiferGit@users.noreply.github.com>
2019-04-19 21:54:17 +0600

    Navigation Added

--- app/src/main/java/com/example/myapplication/DealerMainActivity.java
+++ app/src/main/java/com/example/myapplication/DealerMainActivity.java
@@ -1,15 +1,29 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class DealerMainActivity extends AppCompatActivity {
 
     private Button card_punch;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    private CircleImageView navProfileImage;
+    private TextView navProfileUserName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +34,20 @@ public class DealerMainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Dealer Home");
 
 
+        //adding navigation view in drawer layout
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(DealerMainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+        navProfileImage = (CircleImageView) navView.findViewById(R.id.nav_profile_image);
+        navProfileUserName = (TextView) navView.findViewById(R.id.nav_user_fullname);
+
+
+
         card_punch = (Button) findViewById(R.id.dealer_main_punchButton);
         card_punch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +56,14 @@ public class DealerMainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
